d504bce9f62d8665cd1e467f7d4e614a70bdb4a6
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-02-06 16:01:16 +0300

    IDistributedCache-2 (Complex Type’ların cachlenmesi)

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using IDistributedCacheRedisApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
 {
@@ -18,20 +22,42 @@ namespace IDistributedCacheRedisApp.Web.Controllers
         {
             DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions
             {
-                AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(1)
+                AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(30)
             };
 
             //_distributedCache.SetString("name", "Cemal", cacheEntryOptions);
-            await _distributedCache.SetStringAsync("name", "Göktuğ");
+            //await _distributedCache.SetStringAsync("name", "Göktuğ");
+
+            var product = new Product()
+            {
+                Id = 1,
+                Name = "Kitap",
+                Price = 50
+            };
+
+            string jsonProduct1 = JsonConvert.SerializeObject(product);
+            //await _distributedCache.SetStringAsync("product:1", jsonProduct1, cacheEntryOptions);
+
+            var byteProduct = Encoding.UTF8.GetBytes(jsonProduct1);
+
+            await _distributedCache.SetAsync("product:1", byteProduct, cacheEntryOptions);
 
             return View();
         }
 
         public IActionResult Show()
         {
-            var name = _distributedCache.GetString("name");
-            ViewBag.name = name;
-            return View();
+            //var name = _distributedCache.GetString("name");
+            //ViewBag.name = name;
+
+            var jsonProduct = _distributedCache.Get("product:1");
+
+            //var product = JsonConvert.DeserializeObject<Product>(jsonProduct);
+            var byteProduct
+                = Encoding.UTF8.GetString(jsonProduct);
+            var product = JsonConvert.DeserializeObject<Product>(byteProduct);
+
+            return View(product);
         }
 
         public IActionResult Remove()
