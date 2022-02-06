c33ce6d815f6dee88fd0a1ac79be32f080239ae5
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-02-06 16:50:58 +0300

    30. StackExchange.Redis API / Redis String

---
+++ RedisExchangeAPI.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase _db;
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb(0);
+        }
+
+        public IActionResult Index()
+        {
+
+            _db.StringSet("name", "Cemal Bulak");
+            _db.StringSet("ziyaretci", 100);
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            var value = _db.StringGet("name");
+            //var value = _db.StringGetRange("name", 0, 3);
+            //var value = _db.StringLength("name");
+            //_db.StringIncrement("ziyaretci", 1);
+            //_db.StringIncrementAsync("ziyaretci", 1);
+
+            //_db.StringDecrementAsync("ziyaretci");
+
+            if (value.HasValue)
+            {
+                ViewBag.name = value.ToString();
+                //ViewBag.ziyaretci = _db.StringGet("ziyaretci");
+                ViewBag.ziyaretci = _db.StringDecrementAsync("ziyaretci").Result;
+            }
+
+            return View();
+        }
+    }
+}
