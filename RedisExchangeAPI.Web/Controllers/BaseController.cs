1e47b54ab2fcfa425c96ccb9c10f77ea6918e727
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-02-20 18:31:02 +0300

    34. StackExchange.Redis API / Redis Hash

---
+++ RedisExchangeAPI.Web/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        public readonly RedisService _redisService;
+        public readonly IDatabase _db;
+
+        public BaseController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb(1);
+        }
+
+    }
+}
---
+++ RedisExchangeAPI.Web/Controllers/HashTypeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class HashTypeController : BaseController
+    {
+        private string HashKey { get; set; } = "hashKey";
+
+        public HashTypeController(RedisService redisService) : base(redisService)
+        {
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            Dictionary<string, string> list = new Dictionary<string, string>();
+
+            if (_db.KeyExists(HashKey))
+            {
+                _db.HashGetAll(HashKey).ToList().ForEach(x => { list.Add(x.Name, x.Value); });
+            }
+
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, string val)
+        {
+            _db.HashSet(HashKey, name, val);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteItem(string name)
+        {
+            _db.HashDelete(HashKey, name);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
